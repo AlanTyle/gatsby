AlanTyle/gatsby

commit e75651c0b057e3a2345f2a6dbd458f92545efc0f

Fix prefixes in gatsby-link + navigateTo (#1550)

packages/gatsby-link/src/index.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types"
 
 let pathPrefix = ``
 if (__PREFIX_PATHS__) {
-  pathPrefix = __PATH_PREFIX__
+  pathPrefix = `${__PATH_PREFIX__}/`
 }
 
 class GatsbyLink extends React.Component {
@@ -92,5 +92,5 @@ GatsbyLink.contextTypes = {
 export default GatsbyLink
 
 export const navigateTo = pathname => {
-  window.___navigateTo(pathname)
+  window.___navigateTo(pathPrefix + pathname)
 }

commit 05947934474c83c60ea1d35acad13cf8204ecf54

Remove the slash between the pathPrefix and pathname when navigating (#1574)
* Removes the slash between the `pathPrefix` and `pathname` when navigating - this bug was introduced by (#1550)

* Fix unsupported method in IE (#1573)

* Fix unsupported method in IE

* Fix formatting

* Don't set a default title in html.js as not overriden by react-helmet (#1578)

* Downgrade Glamor to v2 as v3 unstable (#1580)

* Downgrade Glamor to v2 as v3 unstable

* Fixes

* Fix other PR

* Remove replace

packages/gatsby-link/src/index.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types"
 
 let pathPrefix = ``
 if (__PREFIX_PATHS__) {
-  pathPrefix = `${__PATH_PREFIX__}/`
+  pathPrefix = __PATH_PREFIX__
 }
 
 class GatsbyLink extends React.Component {

packages/gatsby-plugin-glamor/src/gatsby-ssr.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { renderToString } from "react-dom/server"
-const { renderStaticOptimized } = require("glamor/server")
+const { renderStaticOptimized } = require(`glamor/server`)
 
 exports.replaceRenderer = ({
   bodyComponent,